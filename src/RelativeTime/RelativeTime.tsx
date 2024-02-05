e3dc7e9b2987f0f041d5d18ad1ab95c56c5d6294
Keith Cirkel <keithamus@users.noreply.github.com>
2024-02-05 12:27:43 +0000

    try to make relativetime output dates in ssr context

--- src/RelativeTime/RelativeTime.tsx
+++ src/RelativeTime/RelativeTime.tsx
@@ -5,7 +5,17 @@ import {createComponent} from '@lit-labs/react'
 import {ComponentProps} from '../utils/types'
 import sx from '../sx'
 
-const RelativeTime = styled(createComponent(React, 'relative-time', RelativeTimeElement))(sx)
+const RelativeTimeComponent = styled(createComponent(React, 'relative-time', RelativeTimeElement))(sx)
 
-export type RelativeTimeProps = ComponentProps<typeof RelativeTime>
+export type RelativeTimeProps = ComponentProps<typeof RelativeTimeComponent>
+
+const localeOptions: Intl.DateTimeFormatOptions = {month: 'short', day: 'numeric', year: 'numeric'}
+function RelativeTime(props: RelativeTimeProps) {
+  return (
+    <RelativeTimeComponent {...props}>
+      ${props.date?.toLocaleDateString('en', localeOptions) || ''}
+    </RelativeTimeComponent>
+  )
+}
+
 export default RelativeTime
